2e7ce2c73f9eaecf4fa22a6dfe4a221363badb63
dhhhead67-create <dhhhead67@gmail.com>
2026-06-22 19:23:59 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Modding in animal company",
+        name: "MODING IN AC",
         // Activity type number (0 = Playing).
         type: 0,
       },
